cdb2d595a2cc98a401d88dd45dccea3da88721f8
Mutthuram03 <srmutthuram2007@gmail.com>
2026-03-22 17:11:59 +0530

    style: enhance hero background visuals

---
+++ src/components/HeroBackground.jsx
@@ -0,0 +1,9 @@
+const HeroBackground = () => {
+  return (
+    <div className="absolute inset-0 w-full h-full pointer-events-none">
+      {/* Background removed to allow global pure black theme */}
+    </div>
+  );
+};
+
+export default HeroBackground;
