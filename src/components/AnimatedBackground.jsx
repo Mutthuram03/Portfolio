6475aa19d34c8a4919dc4aef851a5f9e58645763
Mutthuram03 <srmutthuram2007@gmail.com>
2026-03-24 00:01:36 +0530

    style: add animated background effects

---
+++ src/components/AnimatedBackground.jsx
@@ -0,0 +1,9 @@
+const AnimatedBackground = () => {
+  return (
+    <div className="absolute inset-0 pointer-events-none overflow-hidden">
+      {/* Background removed to allow global pure black theme */}
+    </div>
+  );
+};
+
+export default AnimatedBackground;
