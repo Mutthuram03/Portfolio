1d0e0916456946f77c07aa81ea25917256a6194e
Mutthuram03 <srmutthuram2007@gmail.com>
2026-03-24 00:02:08 +0530

    style: add section background styling

---
+++ src/components/SectionBackground.jsx
@@ -0,0 +1,9 @@
+const SectionBackground = () => {
+  return (
+    <div className="fixed inset-0 w-full h-full pointer-events-none">
+      {/* Background removed to allow global pure black theme */}
+    </div>
+  );
+};
+
+export default SectionBackground;
