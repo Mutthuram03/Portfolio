b56e29a725520293f7acb38d87c33efd9299c84f
Mutthuram03 <srmutthuram2007@gmail.com>
2026-03-24 00:14:07 +0530

    feat: update theme configuration to default light mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { ThemeProvider } from "next-themes";
 import App from "./App.jsx";
 import "./index.css";
 
+// Force light mode on initial load by removing any stored preference
+localStorage.removeItem("theme");
+
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+  <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
     <App />
   </ThemeProvider>
 );
